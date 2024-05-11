5a0de2f08d2dfdcc99eb3fbd7fad584caedad74b
mateusz_tabor <mattab.dev@gmail.com>
2024-05-11 14:04:57 +0200

    changes to lab2

--- wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/PatientDAO.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/PatientDAO.java
@@ -2,5 +2,8 @@ package com.capgemini.wsb.persistence.dao;
 
 import com.capgemini.wsb.persistence.entity.PatientEntity;
 
+import java.util.List;
+
 public interface PatientDAO extends Dao<PatientEntity, Long> {
+    List<PatientEntity> findPatientBySurname(final String surname);
 }
--- wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/impl/PatientDAOImpl.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/impl/PatientDAOImpl.java
@@ -4,7 +4,13 @@ import com.capgemini.wsb.persistence.dao.PatientDAO;
 import com.capgemini.wsb.persistence.entity.PatientEntity;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class PatientDAOImpl extends AbstractDao<PatientEntity, Long> implements PatientDAO{
-
+    @Override
+    public List<PatientEntity>findPatientBySurname(String surname) {
+        return entityManager.createQuery("select p from PatientEntity p where p.surname = :surname")
+                .setParameter("surname", surname).getResultList();
+    }
 }
